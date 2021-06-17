77c0d105a33f87c01d9d18595b16e7156428848a
Tarbo <mellitus4u@gmail.com>
2021-06-17 08:42:22 -0400

    Unit Test: Added the can add item test

--- tests/TestCheckOut.py
+++ tests/TestCheckOut.py
@@ -9,3 +9,21 @@ from pytest import fixture
 def checkout() -> object:
     checkout = CheckOut()
     return checkout
+
+
+def test_can_add_item_price(checkout) -> None:
+    """
+        Test case for can add item price.
+    Returns:
+        None
+    """
+    checkout.add_item_price("item", 56)
+
+
+def test_add_item(checkout) -> None:
+    """
+        Test add item method.
+    Returns:
+        None
+    """
+    checkout.add_item("item")
