71963dce3b8b3ff982575fc8db16768cba32c2bd
Tarbo <mellitus4u@gmail.com>
2021-06-17 08:44:35 -0400

    Unit Test: Added the class methods for adding price, item and current total

---
+++ src/CheckOut.py
@@ -0,0 +1,39 @@
+"""
+    Check out class.
+"""
+
+
+class CheckOut:
+    pass
+
+    def add_item_price(self, item: str, price: float) -> None:
+        """
+            A method to add price to item.
+        Args:
+            item (str):
+            price (float):
+        Returns:
+            None
+        """
+        pass
+
+    def add_item(self, item: str) -> None:
+        """
+            A method to add item.
+        Args:
+            item (str):
+        Returns:
+            None
+        """
+        pass
+
+    def calculate_current_total(self, previous_total: float, new_item_price: float) -> None:
+        """
+            Test if we can compute current total.
+        Args:
+            previous_total (float):
+            new_item_price (float):
+        Returns:
+            None
+        """
+        pass
