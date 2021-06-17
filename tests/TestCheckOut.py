a3850926b364242b369f126bde3412885da5c311
Tarbo <mellitus4u@gmail.com>
2021-06-17 08:43:50 -0400

    Unit Test: Added the calculate current total test

--- tests/TestCheckOut.py
+++ tests/TestCheckOut.py
@@ -27,3 +27,14 @@ def test_add_item(checkout) -> None:
         None
     """
     checkout.add_item("item")
+
+
+def test_calculate_current_total(checkout) -> None:
+    """
+        Test whether we can calculate current total.
+    Args:
+        checkout (object):
+    Returns:
+        None
+    """
+    checkout.calculate_current_total(35, 23)
