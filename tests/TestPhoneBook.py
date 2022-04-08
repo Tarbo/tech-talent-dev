837a34b93fb738a69d4fab6c0519c3f8e488a277
tarbo <mellitus4u@gmail.com>
2022-04-07 23:37:20 -0400

    Phonebook: Add contact method passed

--- tests/TestPhoneBook.py
+++ tests/TestPhoneBook.py
@@ -1,14 +1,16 @@
 import unittest
 
+from src.PhoneBook import PhoneBook
+
 
 class TestPhoneBook(unittest.TestCase):
-    def test_add_contact(self)->None:
+    def test_add_contact(self) -> None:
         """
             A method to test the add contact method of phonebook.
         Returns:
             None
         """
-        phonebook=PhoneBook()
-        phonebook.add_contact('Tarbo','647-678-1352')
-        number=phonebook.get_contact_by_name('Tarbo')
-        self.assertEqual('647-678-1352',number)
+        phonebook = PhoneBook()
+        phonebook.add_contact('Tarbo', '647-678-1352')
+        number = phonebook.contact.get('Tarbo')
+        self.assertEqual('647-678-1352', number)
