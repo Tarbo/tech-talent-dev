c2f5bd3c082d2e421b2cef59040b196e810e047b
tarbo <mellitus4u@gmail.com>
2022-04-07 23:56:26 -0400

    Phonebook: Add test method for get contact

--- tests/TestPhoneBook.py
+++ tests/TestPhoneBook.py
@@ -14,3 +14,14 @@ class TestPhoneBook(unittest.TestCase):
         phonebook.add_contact('Tarbo', '647-678-1352')
         number = phonebook.contact.get('Tarbo')
         self.assertEqual('647-678-1352', number)
+
+    def test_get_contact(self) -> None:
+        """
+            Check whether a key error is raised for invalid name
+        Returns:
+            None
+        """
+        phonebook = PhoneBook()
+        phonebook.contact['Nneka'] = '5678'
+        number = phonebook.get_contact('Nneka')
+        self.assertEqual('5678', number)
